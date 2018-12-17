f9e8794a5d03d6ae1108be7e502f04f5cc9fc94f
vijendra singh <adroit.k10@gmail.com>
2018-12-17 16:38:56 +0530

    created a function to concatenate element of list into string

---
+++ week1/9. WAP to concatenate all elements in a list into a string and return it.py
@@ -0,0 +1,16 @@
+"""
+Write a program to concatenate all elements of list into a string
+
+Author : vijendra pratap singh
+Email : pratap.vijendrasingh96@gmail.com
+"""
+
+def concatelist(list_elements):
+    return "".join(list_elements)
+
+list_elem = ['q','w','e','r','t','y','u','i','o','p']
+str_elem = concatelist(list_elem)
+print(str_elem)
+
+
+
